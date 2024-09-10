02fb0804cee6dc80d2715e145ff9910611c13477
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-10 16:28:20 +0530

    nav bar updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "@/src/components/NavBar";
 
 export const metadata: Metadata = {
   title: "Segment",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NavBar/>
         {children}
       </body>
     </html>
