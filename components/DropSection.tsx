be0955aff9f9f284047dc40b6a66c8f5dedc7fcf
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-11 15:10:13 +0530

    supabase client and utils updated

--- components/DropSection.tsx
+++ components/DropSection.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+'use client';
+
+import React,{useRef, useState} from 'react'
 import Image from 'next/image'
 
 const DropSection = () => {
+    const [SelectedFile, setSelectedFile] = useState(null);
+    const [Uploaded, setUploaded] = useState(null);
+    const inputRef = useRef(null);
   return (
     <div className='flex items-center justify-center md:gap-7 gap-2 border-2 border-dashed border-sblack border-opacity-10 h-[160px] w-full rounded-md cursor-pointer relative'>
-        <input accept='*' type="file" multiple className='absolute w-full h-full m-0 p-0 cursor-pointer outline-none opacity-0' />
+        <input accept='*' type="file" className='absolute w-full h-full m-0 p-0 cursor-pointer outline-none opacity-0' />
         <div>
-            <Image src='/icons/upload.svg' height={40} width={40} alt='drop'/>
+            <Image src='/icons/upload.svg' height={35} width={35} alt='drop'/>
         </div>
         <div className='font-poppins flex flex-col justify-center items-start gap-1'>
             <h1 className='text-sm text-sblack font-medium'>Import or drap and drop your files here</h1>
