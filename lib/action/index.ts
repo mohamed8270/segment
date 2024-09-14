8974118c4d14c9ad24f0311b44151589ca8b9563
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-14 11:15:32 +0530

    crypto js hashing completed

--- lib/action/index.ts
+++ lib/action/index.ts
@@ -1,12 +1,15 @@
 import supabase from '@/utils/supabase/client';
-import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
+import sha256 from 'crypto-js/sha256';
 
 export async function handleUpload(file: any) {
     if(file) {
         try {
             const bucket: string = "medimages";
+            const nanoid = customAlphabet('1234567890', 6);
             const filename = nanoid();
             console.log(`This is file ${file}`, filename);
+            handleHashing(file);
             const {data, error} = await supabase.storage.from(bucket).upload(`${filename}.${file.name.split(".").pop()}`, file);
             if(error) { 
                 console.log('Error while uploading', error.message);
@@ -22,3 +25,13 @@ export async function handleUpload(file: any) {
     return 'Client side issue!';
 } 
 
+export async function handleHashing(file: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        const fileContent = e.target?.result;
+        const hashed = sha256(fileContent as string).toString();
+        console.log(`This is hased ${hashed}`);
+        return hashed;
+    };
+    reader.readAsArrayBuffer(file);
+}
