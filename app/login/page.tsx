6b69b887d0f3f87e8d27bd249a469d6c38f2c7d9
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-17 16:58:42 +0530

    id assigned to signup page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 const LoginPage = () => {
   return (
     <div>
-        
+        <div className='flex-1'>
+            <img src="/images/gradient-1.jpg" alt="login" className='h-screen object-cover' />
+        </div>
+        <div>
+            <h1>Welcome back,</h1>
+            <p>Tip: Users can store potential data such as medical data securely here</p>
+            
+        </div>
     </div>
   )
 }
