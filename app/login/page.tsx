fd63cb450bb16ea8c6ff0f6ee21c7a1eb9abb3cd
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-17 17:30:13 +0530

    login page frontend created

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,26 @@
+'use client';
+
 import React from 'react'
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import Link from 'next/link';
 
 const LoginPage = () => {
+    const [loading, setLoading] = React.useState(false);
+    const [user, setUser] = React.useState({email: '', password: ''});
+    const onLogIn = async () => {}
   return (
-    <div>
-        <div className='flex-1'>
-            <img src="/images/gradient-1.jpg" alt="login" className='h-screen object-cover' />
+    <div className='md:flex justify-center items-center gap-10 mx-6'>
+        <div className='flex-1 justify-end items-center md:ml-16'>
+            <h1 className='text-3xl font-poppins font-bold text-sblack'>Welcome back,</h1>
+            <p className='text-xs font-regular font-poppins text-sblack my-5'>Tip: Users can store potential data such as medical data securely here</p>
+            <input id='email' value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} type="mail" placeholder='Email' className='h-12 md:w-[464px] w-full bg-sgrey bg-opacity-70 text-xs font-poppins font-regular text-sblack outline-none pl-2 rounded-md' />
+            <input id='password' value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} type="password" placeholder='Password' className='my-3 h-12 md:w-[464px] w-full bg-sgrey bg-opacity-70 text-xs font-poppins font-regular text-sblack outline-none pl-2 rounded-md' />
+            <button className='h-12 md:w-[464px] w-full bg-sblack rounded-md text-xs font-poppins font-medium text-swhite'>LogIn</button>
+            <h1 className='text-xs font-poppins font-regular text-sblack text-opacity-40 my-4'>Dont't have an account? <Link href='/signup'><span className='text-sblack'>Signup here</span></Link> </h1>
         </div>
-        <div>
-            <h1>Welcome back,</h1>
-            <p>Tip: Users can store potential data such as medical data securely here</p>
-            
+        <div className='flex-1 justify-start items-center'>
+            <img src="/images/gradient-1.jpg" alt="login" className='md:h-screen object-cover' />
         </div>
     </div>
   )
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 const SignupPage = () => {
     const router = useRouter();
     const [user, setUser] = React.useState({firstname: "", lastname: "", username: "", email: "", phone: "", password: ""});
-    const SignUp = async () => {
+    const onSignUp = async () => {
         try {
             const signupRoute = String(process.env.SIGNUP_ROUTE);
             const res = await axios.post(signupRoute);
@@ -37,11 +37,11 @@ const SignupPage = () => {
                 <input id='phone' value={user.phone} onChange={(e) => setUser({...user, phone: e.target.value})} type="phone" placeholder='Phone' className='h-12 md:w-[464px] w-full bg-sgrey bg-opacity-70 text-xs font-poppins font-regular text-sblack outline-none pl-2 rounded-md' />
                 <input id='password' value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} type="password" placeholder='Password' className='my-3 h-12 md:w-[464px] w-full bg-sgrey bg-opacity-70 text-xs font-poppins font-regular text-sblack outline-none px-2 rounded-md' />
             </div>
-            <button onClick={SignUp} className='h-12 md:w-[464px] w-full bg-sblack rounded-md text-xs font-poppins font-medium text-swhite'>Sign Up</button>
-            <h1 className='text-xs font-poppins font-regular text-sblack text-opacity-40 my-4'>Already have an account? <Link href='/'><span className='text-sblack'>Login here</span></Link> </h1>
+            <button onClick={onSignUp} className='h-12 md:w-[464px] w-full bg-sblack rounded-md text-xs font-poppins font-medium text-swhite'>Sign Up</button>
+            <h1 className='text-xs font-poppins font-regular text-sblack text-opacity-40 my-4'>Already have an account? <Link href='/login'><span className='text-sblack'>Login here</span></Link> </h1>
         </div>
         <div className='md:flex-1 justify-end items-center md:m-6 rounded-lg'>
-            <img src="/images/gradient.jpg" alt="signup" className='md:h-screen rounded-2xl object-cover'  />
+            <img src="/images/gradient.jpg" alt="signup" className='md:h-screen object-cover'  />
         </div>
     </div>
   )
