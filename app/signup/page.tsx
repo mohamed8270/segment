e5694b2c4bae63c3a6b4e0c9db08ae51740f0c91
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-17 18:13:10 +0530

    #UI changes in signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -19,13 +19,13 @@ const SignupPage = () => {
         }
     }
   return (
-    <div className='md:flex justify-center items-center md:min-h-screen mx-6'>
-        <div className='md:flex-1 justify-start items-center md:pl-12'>
+    <div className='md:flex justify-center items-center gap-10 mx-6'>
+        <div className='md:flex-1 justify-end items-center md:ml-12'>
             <div className='flex items-center gap-2 my-12'>
               <Image src='/icons/cloud-logo.svg' alt='logo' width={26} height={26} className='fill-sblack' />
               <p className='text-md text-sblack font-poppins font-regular'>Segment</p>
             </div>
-            <h1 className='md:text-3xl sm:text-2xl font-poppins font-bold text-sblack leading-relaxed'>Keep your potential data <span className='text-sorange'>organized</span> and <span className='text-sorange'>secured!</span></h1>
+            <h1 className='text-3xl font-poppins font-bold text-sblack leading-relaxed'>Keep your potential data <span className='text-sorange'>organized</span> and <span className='text-sorange'>secured!</span></h1>
             <p className='text-xs font-poppins font-regular text-sblack text-opacity-40 my-5'>Our web application is used to store potential data which is most secured</p>
             <div className='mt-[40px] flex-row'>
                 <div className='flex justify-start items-center gap-4'>
@@ -40,7 +40,7 @@ const SignupPage = () => {
             <button onClick={onSignUp} className='h-12 md:w-[464px] w-full bg-sblack rounded-md text-xs font-poppins font-medium text-swhite'>Sign Up</button>
             <h1 className='text-xs font-poppins font-regular text-sblack text-opacity-40 my-4'>Already have an account? <Link href='/login'><span className='text-sblack'>Login here</span></Link> </h1>
         </div>
-        <div className='md:flex-1 justify-end items-center md:m-6 rounded-lg'>
+        <div className='flex-1 justify-start items-center'>
             <img src="/images/gradient.jpg" alt="signup" className='md:h-screen object-cover'  />
         </div>
     </div>
