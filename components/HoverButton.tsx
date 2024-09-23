288d812223de05a75590f1e3f140d40de6f932e0
Mohamed Ibrahim <55456401+mohamed8270@users.noreply.github.com>
2024-09-23 11:15:15 +0530

    qr page building

--- components/HoverButton.tsx
+++ components/HoverButton.tsx
@@ -12,13 +12,17 @@ interface Props {
 
 const HoverButton = ({path, name, id}: Props) => {
     const [hover, setHover] = useState(false);
+
+    const copy = (e: any) => {
+        navigator.clipboard.writeText(path);
+    }
   return (
     <>
         <button onClick={() => setHover(!hover)} className='bg-sgrey bg-opacity-50 flex justify-center items-center p-2 rounded-md'>
             <Image src='/icons/edit.svg' alt='doc' height={25} width={25}/>
         </button>
         {hover ? <div className='flex items-center gap-1 md:gap-3'>
-            <button className='bg-sgrey bg-opacity-50 flex justify-center items-center p-2.5 rounded-full'>
+            <button onClick={() => copy} className='bg-sgrey bg-opacity-50 flex justify-center items-center p-2.5 rounded-full'>
                 <Image src='icons/copy.svg' alt='copy' height={20} width={20}/>
             </button>
             <button onClick={() => deleteMongoData(id, path)} className='bg-sgrey bg-opacity-50 flex justify-center items-center p-2.5 rounded-full'>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,11 +5,17 @@ import Image from 'next/image'
 const NavBar = () => {
   return (
     <header className='w-full bg-swhite'>
-        <nav className='h-[60px] sm:px-16 px-6 flex items-center'>
-          <Link href='/'>
+        <nav className='h-[60px] sm:px-16 px-6 flex items-center justify-between'>
+          <Link href='/home'>
             <div className='flex items-center gap-2'>
               <Image src='/icons/cloud-logo.svg' alt='logo' width={26} height={26} className='fill-sorange' />
               <p className='text-md text-sblack font-poppins font-semibold'>Segment</p>
+            </div>
+          </Link>
+          <Link href='/qrcode'>
+            <div className='flex items-center justify-center gap-3 h-10 w-36 border-2 border-sgrey rounded-full'>
+              <Image src='/icons/qrcode.svg' alt='qrcode' height={20} width={20} className='fill-sorange'/>
+              <p className='text-xs text-sblack font-poppins font-semibold'>Scan QR</p>
             </div>
           </Link>  
         </nav>
